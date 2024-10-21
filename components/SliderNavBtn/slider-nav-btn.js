1be02034c9e014cb491711349970f3d9fc965cb0
Mike <mikhail-gankin@yandex.ru>
2024-10-21 21:31:02 +0300

    +++ upd

--- components/SliderNavBtn/slider-nav-btn.js
+++ components/SliderNavBtn/slider-nav-btn.js
@@ -1,6 +1,3 @@
-import prevBtnHtml from './slider-nav-prev-btn.html?raw';
-import nextBtnHtml from './slider-nav-next-btn.html?raw';
-
 export function generateNextButton() {
 	return `<button class="custom-slider-nav-btn slider-bnt-next">
 	<svg xmlns="http://www.w3.org/2000/svg" width="6" height="9" viewBox="0 0 6 9" fill="none">
